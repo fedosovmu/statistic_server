d794acaa43c5ba9a9310189dc7c5d53a16b9d775
fedosovmu <fedosovmu@gmail.com>
2018-05-04 03:35:08 +0500

    spoiler

--- views/site/api.php
+++ views/site/api.php
@@ -7,12 +7,13 @@ use yii\helpers\Html;
 $this->title = 'API';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
+<div class="site-about lead">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
 
-    <code><?= __FILE__ ?></code>
+
+    <p>
+        Запросы нужно отправлять по адресу:
+    </p>
+    <code>site/web/api/index</code>
 </div>
--- views/site/index.php
+++ views/site/index.php
@@ -26,15 +26,46 @@ $this->title = 'My Yii Application';
         background-color: #4CAF50;
         color: white;
     }
+
+    .spoiler-wrap{background:#fff;margin: 0 0 8px;border:1px solid #efefef}
+    .spoiler-head{background:#efefef;cursor:pointer;padding:5px;}
+    .spoiler-body{padding:10px}
+    .spoiler-wrap.disabled .spoiler-body{display:none}
+    .spoiler-wrap.active{border-color:#4CAF50}
+    .spoiler-wrap.active .spoiler-head{background:#4CAF50;color:#fff}
 </style>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        jQuery('.spoiler-head').click(function(){
+            $(this).parents('.spoiler-wrap').toggleClass("active").find('.spoiler-body').slideToggle();
+        })
+    })
+</script>
+
 <div class="site-index">
 
-    <div class="body-content">
+    <div class="body-content lead">
         <h2><b>Привет!</b></h2>
 
-        <p class="lead">На данной странице отображается содержимое базы данных<br>
+        <p>На данной странице отображается содержимое базы данных<br>
             Если хочешь узнать о том как работать с API, перейди на вкладку API</p>
 
+        <div class="spoiler-wrap disabled">
+            <div class="spoiler-head">Описание полей таблицы</div>
+            <div class="spoiler-body">
+                <ul>
+                    <li><b>id</b> - Первсичный ключ</li>
+                    <li><b>date</b> - Дата и время игры, устанавливается автоматически</li>
+                    <li><b>player_name</b> - Никнейм игрока, может быть NULL,
+                        никнейм необезателен так как у каждой игры есьт дата</li>
+                    <li><b>score</b> - Количество очков</li>
+                    <li><b>data</b> - Текстовая строка длиной до 500 символов, можешь хранить
+                        в ней все что захочешь, например длительность матча или сделаные ходы</li>
+                </ul>
+            </div>
+        </div>
 
         <table class="player_table">
             <tr>
@@ -49,9 +80,9 @@ $this->title = 'My Yii Application';
                     echo '<tr>';
                     echo '<td>' . $game['id'] . '</td>';
                     echo '<td>' . $game['date'] . '</td>';
-                    echo '<td>' . $game['player_name'] . '</td>';
+                    echo '<td>' . htmlentities($game['player_name']) . '</td>';
                     echo '<td>' . $game['score'] . '</td>';
-                    echo '<td>' . $game['data'] . '</td>';
+                    echo '<td>' . htmlentities($game['data']) . '</td>';
                     echo '</tr>';
                 }
             ?>
